67ec8184994c6627b7f1cf4dfed4d1b64a7515d4
Rodrigo Amorim <Ramorim13@yahoo.com.br>
2023-07-13 16:22:58 -0300

    Enviando Arquivo De Aulas

--- Javascript/Projeto_Mata_Mosquito/jogo.js
+++ Javascript/Projeto_Mata_Mosquito/jogo.js
@@ -1,5 +1,33 @@
 var largura = 0
 var altura = 0
+var vidas = 1
+var tempo = 15
+var nivel = window.location.search // recuperou o valor apos o ponto de interrogação no link ou seja o valor passado
+var criaMosquitoTempo = 1500
+
+nivel = nivel.replace('?', '')
+
+if(nivel === 'normal'){
+    //1500 milisegundos
+    criaMosquitoTempo = 1500
+}else if( nivel === 'dificil'){
+    //1000 milisegundos
+    criaMosquitoTempo = 1000
+}else if( nivel === 'chucknorris'){
+    //750 milisegundos
+    criaMosquitoTempo = 750
+}
+
+var cronometro = setInterval( function(){
+    tempo -= 1
+    if( tempo < 0){
+        clearInterval(cronometro)
+        clearInterval(criaMosquito)
+        window.location.href = 'vitoria.html'
+    }else{
+        document.getElementById('cronometro').innerHTML = tempo 
+    }
+},1000)
 
 function ajustaTamanhoPalcoJogo(){
      altura = window.innerHeight
@@ -11,6 +39,19 @@ ajustaTamanhoPalcoJogo()
 
 
 function posicaoRandomica(){
+    // remover o mosquito anterior (caso exista)
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+        if(vidas > 3){
+            //game over
+            window.location.href = 'fim_de_jogo.html'
+        } else{
+            document.getElementById('v'+ vidas).src = 'imagens/coracao_vazio.png'
+
+            vidas++
+        }
+    }
+    
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
     //console.log(posicaoX, posicaoY)
@@ -25,6 +66,10 @@ function posicaoRandomica(){
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+    mosquito.onclick = function(){
+        this.remove()
+    }
     document.body.appendChild(mosquito)
 
 }
